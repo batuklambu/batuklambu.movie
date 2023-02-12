18dd21b082298abfa2e8f8f3c7aa9c7fc894d86b
batuklambu <batuklambu@gmail.com>
2023-02-11 21:58:58 -0500

    fetchdata

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,8 +1,30 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import MovieListing from '../components/MovieListing/MovieListing';
+import movieApi from '../common/apis/movieApi';
+// import { APIKey } from '../common/apis/MovieApi.key';
 const Home = () => {
-  return;
-  <div>Home</div>;
+  const movieText = 'Harry';
+  const APIKey = '78fdab31';
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log('Err:', err);
+        });
+      console.log(response);
+    };
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <div className="banner-img">
+        <MovieListing />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
