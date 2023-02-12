347d40a1606e84909f39eaa53e7ddb6c581b79ba
batuklambu <batuklambu@gmail.com>
2023-02-12 11:31:00 -0500

    showsFetch

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,13 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import MovieListing from '../components/MovieListing/MovieListing';
-import { fetchAsyncMovies } from '../features/movies/movieSlice';
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from '../features/movies/movieSlice';
 
 const Home = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
   }, [dispatch]);
 
   return (
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -2,6 +2,8 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import movieApi from '../../common/apis/movieApi';
 
 const movieText = 'Harry';
+const seriesText = 'Friends';
+
 const APIKey = '78fdab31';
 
 export const fetchAsyncMovies = createAsyncThunk(
@@ -16,8 +18,21 @@ export const fetchAsyncMovies = createAsyncThunk(
   }
 );
 
+export const fetchAsyncShows = createAsyncThunk(
+  'movies/fetchAsyncShows',
+  async () => {
+    const response = await movieApi.get(
+      `?apiKey=${APIKey}&s=${seriesText}&type=series`
+    );
+
+    console.log(response);
+    return response.data;
+  }
+);
+
 const initialState = {
   movies: {},
+  shows: {},
   // isLoading: true,
 };
 
@@ -55,10 +70,17 @@ const movieSlice = createSlice({
       .addCase(fetchAsyncMovies.rejected, (state, action) => {
         console.log('rejected');
         // state.isLoading = false;
+      })
+      .addCase(fetchAsyncShows.fulfilled, (state, { payload }) => {
+        console.log('fulfilled');
+        // console.log('Fetched Successfully!');
+        return { ...state, shows: payload };
       });
   },
 });
 
 export const { addMovies } = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
+export const getAllShows = (state) => state.movies.shows;
+
 export default movieSlice.reducer;
