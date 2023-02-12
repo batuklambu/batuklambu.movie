3b8f9d3260671d557ca7eab6dc64a3ac18712e5c
batuklambu <batuklambu@gmail.com>
2023-02-12 14:10:37 -0500

    Finished

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -2,8 +2,11 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { fetchAsyncMovieOrShowDetail } from '../../features/movies/movieSlice';
-import { getSelectedMovieOrShow } from '../../features/movies/movieSlice';
-
+import {
+  getSelectedMovieOrShow,
+  removeSelectedMovieOrShow,
+} from '../../features/movies/movieSlice';
+import './MovieDetail.css';
 const MovieDetail = () => {
   const { imdbID } = useParams();
   const dispatch = useDispatch();
@@ -11,53 +14,63 @@ const MovieDetail = () => {
   console.log(data);
   useEffect(() => {
     dispatch(fetchAsyncMovieOrShowDetail(imdbID));
+    return () => {
+      dispatch(removeSelectedMovieOrShow());
+    };
   }, [dispatch, imdbID]);
 
   return (
-    <div className='"movie-section'>
-      <div className="section-left">
-        <div className="title">{data.title}</div>
-        <div className="movie-rating">
-          <span>
-            IMDB Rating <i className="fa fa-star"></i> :{data.imdbRating}
-          </span>
-          <span>
-            IMDB Votes <i className="fa fa-thums-up"></i> :{data.imdbVotes}
-          </span>
-          <span>
-            IMDB Runtime <i className="fa fa-film"></i> :{data.Runtime}
-          </span>
-          <span>
-            IMDB Year <i className="fa fa-calendar"></i> :{data.Year}
-          </span>
-        </div>
-        <div className="movie-plot">{data.Plot}</div>
-        <div className="movie-info">
-          <div>
-            <span>Director</span>
-            <span>{data.Director}</span>
+    <div className="movie-section">
+      {Object.keys(data).length === 0 ? (
+        <div>...isLoading</div>
+      ) : (
+        <>
+          <div className="section-left">
+            <div className="movie-title">{data.Title}</div>
+            <div className="movie-rating">
+              <span>
+                IMDB Rating <i className="fa fa-star"></i> : {data.imdbRating}
+              </span>
+              <span>
+                IMDB Votes <i className="fa fa-thumbs-up"></i> :{' '}
+                {data.imdbVotes}
+              </span>
+              <span>
+                Runtime <i className="fa fa-film"></i> : {data.Runtime}
+              </span>
+              <span>
+                Year <i className="fa fa-calendar"></i> : {data.Year}
+              </span>
+            </div>
+            <div className="movie-plot">{data.Plot}</div>
+            <div className="movie-info">
+              <div>
+                <span>Director</span>
+                <span>{data.Director}</span>
+              </div>
+              <div>
+                <span>Stars</span>
+                <span>{data.Actors}</span>
+              </div>
+              <div>
+                <span>Generes</span>
+                <span>{data.Genre}</span>
+              </div>
+              <div>
+                <span>Languages</span>
+                <span>{data.Language}</span>
+              </div>
+              <div>
+                <span>Awards</span>
+                <span>{data.Awards}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <span>Stars</span>
-            <span>{data.Actors}</span>
+          <div className="section-right">
+            <img src={data.Poster} alt={data.Title} />
           </div>
-          <div>
-            <span>Generes</span>
-            <span>{data.Genre}</span>
-          </div>
-          <div>
-            <span>Languages</span>
-            <span>{data.Language}</span>
-          </div>
-          <div>
-            <span>Awards</span>
-            <span>{data.Awards}</span>
-          </div>
-        </div>
-      </div>
-      <div className="section-right">
-        <img src={data.Poster} altlet={data.Title} />
-      </div>
+        </>
+      )}
     </div>
   );
 };
